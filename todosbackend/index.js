170f123d5ccff663d691baa9a7f375adee2b92ee
dev637 <devang713@gmail.com>
2018-11-14 15:38:26 -0500

    Add user schema, error handling, and auth routes

--- todosbackend/index.js
+++ todosbackend/index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const errorHandler = require('./helpers/error');
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-const todoRoutes = require('./routes/todos.js');
+const todoRoutes = require('./routes/todos');
+const authRoutes = require('./routes/auth');
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
@@ -18,7 +22,16 @@ app.get('/', (req, res) => {
   res.sendFile('index.html');
 });
 
+app.use('/api/auth', authRoutes);
 app.use('/api/todos', todoRoutes);
+
+app.use((req, res, next) => {
+  let err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`app is running on ${port}`);
--- todosbackend/models/index.js
+++ todosbackend/models/index.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 mongoose.set('debug', true);
-mongoose.connect(process.env.DATABASE_URI);
+mongoose.connect(process.env.DATABASE_URI, { useNewUrlParser: true });
 
 mongoose.Promise = Promise;
 
 module.exports.Todo = require('./todo');
+module.exports.User = require('./users');
