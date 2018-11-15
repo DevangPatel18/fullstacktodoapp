828b74d8e7c3899f6c6b7a32aedc5d61090bf877
dev637 <devang713@gmail.com>
2018-11-14 19:02:03 -0500

    Add signin function

--- todosbackend/routes/auth.js
+++ todosbackend/routes/auth.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { signup } = require('../helpers/auth');
+const { signup, signin } = require('../helpers/auth');
 
 router.post('/signup', signup);
+router.post('/signin', signin);
 
 module.exports = router;
