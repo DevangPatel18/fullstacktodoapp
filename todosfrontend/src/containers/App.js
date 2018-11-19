51e7e24c7a09f5d362e5ae62a604703998a0f53b
dev637 <devang713@gmail.com>
2018-11-19 14:08:43 -0500

    Add navbar component
    Added navbar component and changed stylesheet to scss.

--- todosfrontend/src/containers/App.js
+++ todosfrontend/src/containers/App.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
 import { configureStore } from '../store';
-import TodoList from '../TodoList';
 import { BrowserRouter as Router } from 'react-router-dom';
+import TodoList from '../TodoList';
+import Navbar from './Navbar';
 
 const store = configureStore();
 
 const App = () => (
   <Provider store={store}>
     <Router>
-      <div className="App">
+      <div className="onboarding">
+        <Navbar />
         <TodoList />
       </div>
     </Router>
---
+++ todosfrontend/src/containers/Navbar.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <Link to="/" className="navbar-brand">
+              <img
+                src="https://res.cloudinary.com/dbeqp2lyo/image/upload/v1542653521/note-42883.svg"
+                alt="Notes!"
+              />
+            </Link>
+          </div>
+        <ul className="nav navbar-nav navbar-right">
+          <li>
+            <Link to="/signup">Sign up</Link>
+          </li>
+          <li>
+            <Link to="/signin">Log in</Link>
+          </li>
+        </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    currentUser: state.currentUser,
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  null
+)(Navbar);
